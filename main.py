fe8e026af45977b902e138fbb8661500207e1ae8
LdPx <tilman.schramke@stud.hn.de>
2017-05-28 01:02:01 +0200

    Damn you Async!
    Damn you!!!

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import logging
+from twisted.internet import reactor
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+
+from scrapy.utils.project import get_project_settings
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
+
 
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
@@ -15,12 +23,55 @@ class BlogSpider(scrapy.Spider):
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
+
+def spider_results():
+    results = []   
+
+    def crawler_results(parse_result):
+        results.append(parse_result)
+
+    dispatcher.connect(crawler_results)
+
+    runner = CrawlerRunner(get_project_settings())
+    running_crawler = runner.crawl(BlogSpider)
+    running_crawler.addBoth(lambda _: reactor.stop())
+    reactor.run()
+
+
 if __name__ == '__main__':
+#    logging.getLogger('scrapy').setLevel(logging.CRITICAL) # ???
+    logging.getLogger('scrapy').propagate = False
+    
     print("Hello. My Name is scraper.")
     
-    process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
+    '''    
+    configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+    runner = CrawlerRunner()
+    
+    d = runner.crawl(BlogSpider)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run() # script blocks here until crawling is finished
+    '''    
+   
+    spider_results()
+    
+#    process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
 
-    process.crawl(BlogSpider)
-    process.start() # script blocks here until crawling is finished
+#    process.crawl(BlogSpider)
+#    process.start() # script blocks here until crawling is finished
     
     print("Bye. My Name was scraper.")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

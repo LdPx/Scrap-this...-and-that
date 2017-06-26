7372073e68b8248354153e15b6cf2a0710e7e4c0
LdPx <tilman.schramke@stud.hn.de>
2017-06-27 00:20:04 +0200

    2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import time
 import logging
 from twisted.internet import reactor
 import scrapy
@@ -9,26 +8,16 @@ from scrapy.utils.log import configure_logging
 from tutorial.spiders.c_pull_spider import CSpider
 
 if __name__ == '__main__':
-#    logging.getLogger('scrapy').setLevel(logging.CRITICAL) # ???
     logging.getLogger('scrapy').propagate = False
-    
-    print("Hello. My Name is scraper.")
     
     configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
     runner = CrawlerRunner()
     
-    d = runner.crawl(CSpider)
+    d = runner.crawl(CSpider)   # Class name - with CMD: 'name' attribute
     d.addBoth(lambda _: reactor.stop())
-    reactor.run() # script blocks here until crawling is finished
+    reactor.run()
     
-   
-#    process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
-
-#    process.crawl(BlogSpider)
-#    process.start() # script blocks here until crawling is finished
-    
-#    time.sleep(5)
-    print("Bye. My Name was scraper.")
+    print("Finished.")
     
     
     
